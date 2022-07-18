f2ee366b3d44ee7738ff97cafbf05352bbe121ea
gaokejigaokeji <105533235+gaokejigaokeji@users.noreply.github.com>
2022-07-18 10:02:20 -0700

    Update NBody.java

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -14,12 +14,12 @@ public class NBody{
 		for (int i = 0; i < planetInFile.length; i++){
 			planetInFile[i].draw();
 		}
-		StdOut.printf("%d\n", planets.length);
-		StdOut.printf("%.2e\n", radius);
-		for (int i = 0; i < planets.length; i++) {
+		StdOut.printf("%d\n", planetInFile.length);
+		StdOut.printf("%.2e\n", univseRadius);
+		for (int i = 0; i < planetInFile.length; i++) {
     			StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                  		planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
-                  		planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+                  		planetInFile[i].xxPos, planetInFile[i].yyPos, planetInFile[i].xxVel,
+                  		planetInFile[i].yyVel, planetInFile[i].mass, planetInFile[i].imgFileName);   
 		}
 		
 	}
