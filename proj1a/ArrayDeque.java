3f64fa117138fa92f0a17c3813b03eda75ebc0f3
gaokejigaokeji <105533235+gaokejigaokeji@users.noreply.github.com>
2022-07-28 16:47:57 -0700

    Add files via upload

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,7 +1,7 @@
 /** Performs some basic linked list tests. */
 public class ArrayDeque <T> {
 	private int size;
-	public T[] array; 
+	public T[] array;
 	private int nextFirst;
 	private int nextLast;
 
@@ -13,27 +13,27 @@ public class ArrayDeque <T> {
 		nextLast = 1;
 	}
 
-	
+
 	public void addFirst(T item) {
 		array[nextFirst] = item;
-		if (nextFirst == 0){
+		if (nextFirst == 0) {
 			nextFirst = array.length - 1;
-		}else{
+		} else {
 			nextFirst -= 1;
 		}
-		
+
 		size += 1;
 
 	}
 
 	public void addLast(T item) {
 		array[nextLast] = item;
-		if (nextLast == array.length - 1){
+		if (nextLast == array.length - 1) {
 			nextLast = 0;
-		}else{
+		} else {
 			nextLast += 1;
 		}
-		
+
 		size += 1;
 	}
 
@@ -41,43 +41,44 @@ public class ArrayDeque <T> {
 		return size == 0;
 	}
 
-    public int size(){
+	public int size() {
 		return size;
 	}
 
-    public void printDeque() {
-    	for (int i = 0; i < array.length; i++){
-    		System.out.print(array[i] + " ");
-    	}
-    	
-    }
+	public void printDeque() {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
 
-    public T removeFirst() {
-    	T output = array[nextFirst + 1];
-    	array[nextFirst + 1] = null;
-    	if (nextFirst == array.length - 1){
-    		nextFirst = 0;
-    	}else{
-    		nextFirst += 1;
-    	}
-    	size -= 1;
-    	return output
+	}
 
-    }
+	public T removeFirst() {
+		T output = array[nextFirst + 1];
+		array[nextFirst + 1] = null;
+		if (nextFirst == array.length - 1) {
+			nextFirst = 0;
+		} else {
+			nextFirst += 1;
+		}
+		size -= 1;
+		return output;
 
-    public T removeLast() {
-    	T output = array[nextLast - 1];
-    	array[nextLast - 1] = null;
-    	if (nextLast == 0){
-    		nextLast = array.length - 1;
-    	}else{
-    		nextLast -= 1;
-    	}
-    	size -= 1;
-    	return output
-    }
+	}
 
-    public T get(int index) {
-    	return array[index];
+	public T removeLast() {
+		T output = array[nextLast - 1];
+		array[nextLast - 1] = null;
+		if (nextLast == 0) {
+			nextLast = array.length - 1;
+		} else {
+			nextLast -= 1;
+		}
+		size -= 1;
+		return output;
+	}
 
-    } 
+	public T get(int index) {
+		return array[index];
+
+	}
+}
